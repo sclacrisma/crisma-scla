bcaaebb20c3a2e0b6640eea62b0904981dde3429
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2024-09-10 20:02:48 -0300

    Perguntas testes

--- src/Components/Pages/faq/Perguntas/index.tsx
+++ src/Components/Pages/faq/Perguntas/index.tsx
@@ -12,8 +12,22 @@ export default function Perguntas() {
           <Accordion.Body>A Crisma é o sacramento que nos dá o Espírito Santo, imprime na alma o caráter de soldado de Jesus Cristo e faz-nos perfeitos cristãos.</Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey='1'>
-          <Accordion.Header> Pergunta 2</Accordion.Header>
-          <Accordion.Body>Resposta para a pergunta 2</Accordion.Body>
+          <Accordion.Header> Quem pode ser padrinho/madrinha de Crisma? </Accordion.Header>
+          <Accordion.Body>
+            Para ser padrinho/madrinha de Crisma, a pessoa deve:
+
+            - Ser católica
+
+            - Ter recebido os sacramentos do Batismo, Crisma e Eucaristia
+
+            - Ter no mínimo 16 anos
+
+            - Viver de acordo com a fé da Igreja Católica
+
+            - Ir à Missa aos domingos e dias santos
+
+            - Confessar-se com frequência
+          </Accordion.Body>
         </Accordion.Item>
       </Accordion>
     </div>
