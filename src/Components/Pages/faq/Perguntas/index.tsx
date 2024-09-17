1fe618905e98c97fe3d0fd5f5cf0cb3a2d9ad72a
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2024-09-17 20:11:22 -0300

    usando dados do datoCMS

--- src/Components/Pages/faq/Perguntas/index.tsx
+++ src/Components/Pages/faq/Perguntas/index.tsx
@@ -2,33 +2,23 @@
 import React from 'react'
 import styles from './Perguntas.module.css'
 import { Accordion } from 'react-bootstrap'
+import { pergunta } from '@/infra/types/IPerguntas'
+import Pergunta from './Pergunta/Pergunta'
 
-export default function Perguntas() {
+interface IPerguntas {
+  perguntas: pergunta[]
+}
+
+export default function Perguntas({ perguntas }: IPerguntas) {
+
   return (
     <div className={styles.perguntas} >
       <Accordion>
-        <Accordion.Item eventKey='0'>
-          <Accordion.Header> O que é Crisma? </Accordion.Header>
-          <Accordion.Body>A Crisma é o sacramento que nos dá o Espírito Santo, imprime na alma o caráter de soldado de Jesus Cristo e faz-nos perfeitos cristãos.</Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey='1'>
-          <Accordion.Header> Quem pode ser padrinho/madrinha de Crisma? </Accordion.Header>
-          <Accordion.Body>
-            Para ser padrinho/madrinha de Crisma, a pessoa deve:
-
-            - Ser católica
-
-            - Ter recebido os sacramentos do Batismo, Crisma e Eucaristia
-
-            - Ter no mínimo 16 anos
-
-            - Viver de acordo com a fé da Igreja Católica
-
-            - Ir à Missa aos domingos e dias santos
-
-            - Confessar-se com frequência
-          </Accordion.Body>
-        </Accordion.Item>
+        {perguntas.map((pergunta, index) => {
+          return (
+            <Pergunta key={index} id={pergunta.id} titulo={pergunta.pergunta} texto={pergunta.resposta} />
+          )
+        })}
       </Accordion>
     </div>
   )
--- src/Components/Pages/faq/index.tsx
+++ src/Components/Pages/faq/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import styles from './FaqPage.module.css'
 import Perguntas from './Perguntas'
+import { cmsService } from '@/infra/cms/cmsService'
 
-export default function FaqPage() {
+export const revalidate = 0;
+
+const query = `
+query { allPerguntaFrequentes { id pergunta resposta } } `
+export default async function FaqPage() {
+  const dados = await cmsService({ query, preview: false }).then(data => data)
+  const perguntas = dados.allPerguntaFrequentes
+  const allPerguntaFrequentes = {
+    allPerguntaFrequentes: perguntas
+  }
   return (
     <main className={styles.main}>
-      <Perguntas />
+      <Perguntas perguntas={perguntas} />
+
     </main>
   )
 }
