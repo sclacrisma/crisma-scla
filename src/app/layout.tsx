52c0f2fcc4b1fb9c9ebf8f58864ff5c023ca7ef2
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2024-09-18 22:40:26 -0300

    vercel statistics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/Components/patterns/headers";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +23,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
