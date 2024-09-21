3c5f5cbc0fc9d940c55a102b885770ce634706b1
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2024-09-21 20:30:07 -0300

    Trocando as letras da pagina

--- src/Components/patterns/headers/LinksMenu/index.tsx
+++ src/Components/patterns/headers/LinksMenu/index.tsx
@@ -4,6 +4,12 @@ import Link from 'next/link'
 import styles from './LinksMenu.module.css'
 import { usePathname } from 'next/navigation'
 
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: '400'
+})
 export default function LinksMenu() {
   const menus = [
     { name: "Home", path: '/' },
@@ -19,7 +25,7 @@ export default function LinksMenu() {
         <li key={index} className={path.split("/")[1] === menu.path.split("/")[1] ? styles.active : styles.linkBox}>
           <Link href={menu.path} className={`${styles.link}`}>
             {/* <p className={styles.index}>0{index}</p> */}
-            <p>{menu.name}</p>
+            <p className={poppins.className} >{menu.name}</p>
           </Link>
         </li>
       )
