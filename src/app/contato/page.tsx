3e8302616122b67bac3184afc3b123d47e8eda22
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2024-10-19 17:36:38 -0300

    criando a pagina

---
+++ src/app/contato/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+import Inscricao from "@/Components/Pages/inscricao";
+import { useEffect } from "react";
+
+
+export default function Page() {
+
+
+  return (
+    <>
+      <Inscricao />
+    </>
+  )
+}
+
+
+
+
